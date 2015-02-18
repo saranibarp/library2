a4d7eb0a886722a2518a0326cf29b0d84562d074
Maria Aranibar <saranibarp@outlook.com>
2015-02-18 00:43:55 +0000

    Added item editing views

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,44 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+
+    if @item.save
+      redirect_to items_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+
+    if @item.update_attributes(item_params)
+      redirect_to items_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to items_path
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :completed_on, :item_type_id)
+  end
 end
